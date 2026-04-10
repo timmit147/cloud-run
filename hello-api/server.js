784c196a9f56619bebdedf74a7be110b857d1a72
timmit147 <timmeeuwsen@hotmail.nl>
2026-04-10 13:41:56 +0200

    Remove emoji from Hello World message

--- hello-api/server.js
+++ hello-api/server.js
@@ -2,11 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World 👋" });
+  res.json({ message: "Hello World" });
 });
 
-// Cloud Run gebruikt altijd PORT
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port);
